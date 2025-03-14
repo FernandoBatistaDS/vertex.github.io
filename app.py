db1ca8f22ebe42a418afb52917b625bab881c640
Gabriel Ferreira Rodrigues Penha <80866236+Gabriel4210@users.noreply.github.com>
2025-03-14 09:56:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -387,28 +387,28 @@ if page == "Página Inicial":
                 <p>Telefone: (71) 99210-9164</p>
             </div>
             <div class="team-member">
-                <img src="https://via.placeholder.com/100" alt="Nome 2">
-                <h4>Nome 2</h4>
-                <p><a href="https://www.linkedin.com/in/nome2" target="_blank">LinkedIn</a></p>
-                <p>Telefone: (00) 00000-0000</p>
+                <img src="blob:https://web.whatsapp.com/b053812e-4e0d-4801-a96e-dd61f6105cdd" alt="Carolina Diniz">
+                <h4>Carolina Diniz</h4>
+                <p><a href="https://www.linkedin.com/in/carolina-diniz-2b80701a4" target="_blank">LinkedIn</a></p>
+                <p>Telefone: (31) 99238-0507</p>
             </div>
             <div class="team-member">
-                <img src="https://via.placeholder.com/100" alt="Nome 3">
-                <h4>Nome 3</h4>
-                <p><a href="https://www.linkedin.com/in/nome3" target="_blank">LinkedIn</a></p>
-                <p>Telefone: (00) 00000-0000</p>
+                <img src="blob:https://web.whatsapp.com/ac7e2eba-6209-4133-93fc-8ef7a32bfafa" alt="Fernando Batista">
+                <h4>Fernando Batista</h4>
+                <p><a href="https://www.linkedin.com/in/fernandobatistads" target="_blank">LinkedIn</a></p>
+                <p>Telefone: (11) 98410-1047</p>
             </div>
             <div class="team-member">
-                <img src="https://via.placeholder.com/100" alt="Nome 4">
-                <h4>Nome 4</h4>
-                <p><a href="https://www.linkedin.com/in/nome4" target="_blank">LinkedIn</a></p>
-                <p>Telefone: (00) 00000-0000</p>
+                <img src="blob:https://web.whatsapp.com/89a7d5d9-35ec-441a-b978-a30259445d65" alt="Antônio Brandenberger">
+                <h4>Antônio Brandenberger</h4>
+                <p><a href="https://www.linkedin.com/in/antonio-brandenberger" target="_blank">LinkedIn</a></p>
+                <p>Telefone: (21) 99252-2552</p>
             </div>
             <div class="team-member">
-                <img src="https://via.placeholder.com/100" alt="Nome 5">
-                <h4>Nome 5</h4>
-                <p><a href="https://www.linkedin.com/in/nome5" target="_blank">LinkedIn</a></p>
-                <p>Telefone: (00) 00000-0000</p>
+                <img src="blob:https://web.whatsapp.com/3a20f054-4625-4ed1-b369-5b84eec9fad1" alt="Matheus Marques">
+                <h4>Matheus Marques</h4>
+                <p><a href="https://www.linkedin.com/in/matheus-marques-rodrigues" target="_blank">LinkedIn</a></p>
+                <p>Telefone: (31) 99172-3497</p>
             </div>
             <div class="team-member">
                 <img src="https://via.placeholder.com/100" alt="Nome 6">
